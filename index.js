b11d82766ba0cd4af9baece1b5cc1bd26a5e6816
sabbir347256 <srgamingzone71@gmail.com>
2024-05-30 15:56:54 +0600

    payment section add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.SECRET_KEY)
 const express = require('express');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
@@ -11,8 +12,8 @@ const port = process.env.PORT || 5000;
 //online-group-study
 //5kGZE5JJuRrbHeQ5
 app.use(cors({
-  origin: ['http://localhost:5173','https://online-group-study-67ed0.web.app','https://online-group-study-67ed0.firebaseapp.com'],
-  credentials : true
+  origin: ['http://localhost:5173', 'https://online-group-study-67ed0.web.app', 'https://online-group-study-67ed0.firebaseapp.com'],
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -30,21 +31,21 @@ const client = new MongoClient(uri, {
   }
 });
 
-const logger = (req,res,next) =>{
-  console.log('log : info',req.method,req.url);
+const logger = (req, res, next) => {
+  console.log('log : info', req.method, req.url);
   next();
 }
 
-const tokenVerify = (req,res,next) =>{
+const tokenVerify = (req, res, next) => {
   const token = req?.cookies?.token;
   // console.log('token in the middleware',token);
 
-  if(!token){
-    return res.status(401).send({message : 'unauthorized access'})
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
   }
-  jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded) => {
-    if(err){
-      return res.status(401).send({message : 'unauthorized access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
     }
     req.user = decoded;
     next();
@@ -52,7 +53,7 @@ const tokenVerify = (req,res,next) =>{
 
 }
 
-const cookieOption ={
+const cookieOption = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
@@ -67,17 +68,17 @@ async function run() {
     const CreateAssignment = client.db('OnlineGroupStudyAssignment').collection('assignment');
     const SubmitAssignment = client.db('OnlineGroupStudyAssignment').collection('submitassignment');
 
-    app.post('/jwt',async(req,res) => {
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn : '365d'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '365d' })
 
-      res.cookie('token',token,cookieOption)
-      .send({Success : true})
+      res.cookie('token', token, cookieOption)
+        .send({ Success: true })
     })
 
-    app.post('/logout',async(req,res) => {
+    app.post('/logout', async (req, res) => {
       const user = req.body;
-      res.clearCookie('token',{...cookieOption,maxAge : 0}).send({Success : true})
+      res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ Success: true })
     })
 
     app.get('/feature', async (req, res) => {
@@ -90,9 +91,9 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
-    app.get('/submitAssignment',logger,tokenVerify,  async (req, res) => {
-      if(req.user.email !== req.query.email ){
-        return res.status(403).send({message : 'forbidden access'})
+    app.get('/submitAssignment', logger, tokenVerify, async (req, res) => {
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
       const cursor = SubmitAssignment.find();
       const result = await cursor.toArray();
@@ -118,9 +119,9 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/myassignment',logger,tokenVerify,  async (req, res) => {
-      if(req.user.email !== req.query.email ){
-        return res.status(403).send({message : 'forbidden access'})
+    app.get('/myassignment', logger, tokenVerify, async (req, res) => {
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
       const cursor = SubmitAssignment.find();
       const result = await cursor.toArray();
@@ -186,6 +187,24 @@ async function run() {
       const result = await CreateAssignment.deleteOne(query);
       res.send(result);
     })
+
+    app.post('/create-payment', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntent.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types : ['card']
+      });
+
+      res.send({
+        clientSecret : paymentIntent.client_secret
+      })
+
+    })
+
+
 
 
     // Send a ping to confirm a successful connection
